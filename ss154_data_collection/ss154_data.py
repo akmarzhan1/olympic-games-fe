0a2b63390d92717383dcbd1943bebdf5c0665063
akmarzhan1 <47840436+akmarzhan1@users.noreply.github.com>
2021-04-19 01:26:39 +0600

    init: add new files

---
+++ ss154_data_collection/ss154_data.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import warnings
+warnings.filterwarnings('ignore')
+import pandas as pd
+olympics=pd.read_csv("Downloads/olympic_hosts.csv")
+
+
+# In[2]:
+
+
+olympics.drop(olympics[olympics.Type=='youthgames'].index, inplace=True)
+olympics.drop(olympics[olympics.Year>=2018].index, inplace=True)
+olympics.drop(olympics[olympics.Year<1996].index, inplace=True)
+
+
+# In[3]:
+
+
+olympics = olympics.drop_duplicates(subset=['Country'], keep=False)
+
+
+# In[4]:
+
+
+countries = olympics['Country'].to_numpy()
+
+def generate_id(country):
+    return list(countries).index(country)+1
+
+olympics['id'] = olympics['Country'].apply(generate_id)
+
+
+# In[5]:
+
+
+olympics["Host"] = [1 for i in range(len(countries))]
+olympics["Subtype"] = [1 for i in range(len(countries))]
+olympics['Subtype'] = pd.get_dummies(olympics['Type'])
+olympics = olympics[['Country', 'Year', 'id', 'Subtype', 'Host']]
+olympics
+
+
+# In[6]:
+
+
+years = olympics['Year'].unique()
+
+for country in countries:
+    host_year = olympics[olympics.Country==country].Year.to_numpy()
+    host_id = olympics[olympics.Country==country].id.to_numpy()[0]
+    host_type = olympics[olympics.Country==country].Subtype.to_numpy()[0]
+
+    for year in years:
+        if host_year != year:
+            olympics = olympics.append({'Country': country, 'Year': year, 'id': host_id, 'Subtype': host_type, 'Host': 0}, ignore_index=True)
+
+
+# In[7]:
+
+
+gdpc = pd.read_csv('Downloads/gdp_per_capita.csv')
+olympics['gdpc'] = [0 for i in range(len(olympics))]
+
+fin = pd.read_csv('Downloads/fin_data.csv')
+olympics['FinStability'] = [0 for i in range(len(olympics))]
+
+freedom = pd.read_csv('Downloads/freedom.csv')
+olympics['Freedom'] = [0 for i in range(len(olympics))]
+
+polit = pd.read_csv('Downloads/polit.csv')
+olympics['PolitStability'] = [0 for i in range(len(olympics))]
+
+
+# In[8]:
+
+
+for country in countries:
+    for year in years:
+        idx = olympics.loc[(olympics.Country==country) & (olympics.Year==year)].index
+        olympics['gdpc'][idx] = gdpc[gdpc['Country Name']==country][str(year)].to_numpy()[0]
+
+
+# In[9]:
+
+
+for country in countries:
+    for year in years:
+        idx = olympics.loc[(olympics.Country==country) & (olympics.Year==year)].index
+        olympics['FinStability'][idx] = fin[(fin['country']==country)&(fin['year']==year)]['gfddsi01'].to_numpy()[0]
+
+
+# In[10]:
+
+
+for country in countries:
+    for year in years:
+        idx = olympics.loc[(olympics.Country==country) & (olympics.Year==year)].index
+        olympics['Freedom'][idx] = freedom[(freedom['Name']==country)&(freedom['Index Year']==year)]['Overall Score'].to_numpy()[0]
+
+
+# In[11]:
+
+
+for country in countries:
+    for year in years:
+        idx = olympics.loc[(olympics.Country==country) & (olympics.Year==year)].index
+        olympics['PolitStability'][idx] = polit[(polit['Country Name']==country)&(polit['Series Name']=='Political Stability and Absence of Violence/Terrorism: Estimate')]['{0} [YR{0}]'.format(year)].to_numpy()[0]
+        
+olympics
+
+
+# In[12]:
+
+
+import numpy as np 
+
+diff = []
+olympics['Diff']= [0 for i in range(len(olympics))]
+indices = olympics.index.to_numpy()
+for idx in indices:
+    host_year = olympics[(olympics.Country == olympics.iloc[idx].Country) & (olympics.Host==1)].Year.to_numpy()[0]
+    if olympics.iloc[idx].Host==1:
+        olympics['Diff'][idx]=0
+    else:
+        olympics['Diff'][idx]=olympics.iloc[idx].Year - host_year
+        
+olympics
+
+
+# In[13]:
+
+
+one_hot = pd.get_dummies(olympics[olympics.Diff<=0].Diff, prefix='lead')
+two_hot = pd.get_dummies(olympics[olympics.Diff>=0].Diff, prefix='lag')
+olympics = pd.concat([olympics, one_hot, two_hot], axis=1)
+olympics = olympics.fillna(0)
+
+
+# In[14]:
+
+
+olympics = olympics.sort_values(by=['id', 'Year'])
+olympics
+
+
+# In[15]:
+
+
+olympics.to_csv("Downloads/olympics.csv")
+
